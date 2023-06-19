60fe65b77c0777ce26655c818d49245fb27eebaa
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-06-19 13:24:09 +0200

    update movie app

--- movie-review/src/instruction.rs
+++ movie-review/src/instruction.rs
@@ -7,9 +7,14 @@ pub enum MovieInstruction {
         rating: u8,
         description: String,
     },
+    UpdateMovieReview {
+        title: String,
+        rating: u8,
+        description: String,
+    },
 }
 
-#[devire(BorshDeserialize)]
+#[derive(BorshDeserialize)]
 struct MovieReviewPayload {
     title: String,
     rating: u8,
@@ -19,13 +24,16 @@ struct MovieReviewPayload {
 impl MovieInstruction {
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
         let (&variant, rest) = input.split_first().ok_or(ProgramError::InvalidInstructionData)?;
-
         let payload = MovieReviewPayload::try_from_slice(rest).unwrap();
-
-        // match the first byte and return AddMovieReview struct
         Ok(match variant {
             0 =>
                 Self::AddMovieReview {
+                    title: payload.title,
+                    rating: payload.rating,
+                    description: payload.description,
+                },
+            1 =>
+                Self::UpdateMovieReview {
                     title: payload.title,
                     rating: payload.rating,
                     description: payload.description,
